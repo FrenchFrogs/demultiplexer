0b516eb0d85834c2857964fec2de541c3b734db1
Justine <justine.raze@wengo.com>
2015-11-04 10:45:55 +0100

    :frog: correction migration file

--- database/migrations/create_demultiplexer_table.php
+++ database/migrations/create_demultiplexer_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
+use FrenchFrogs\Laravel\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateDemultiplexerTable extends Migration
@@ -12,17 +12,17 @@ class CreateDemultiplexerTable extends Migration
      */
     public function up()
     {
-        if(!Schema::hasTable('demultiplexer')) {
-            Schema::create('demultiplexer', function(Blueprint $table){
+        $this->down();
 
-                $table->increments('demultiplexer_id');
-                $table->text('params');
-                $table->string('token',30)->unique();
-                $table->boolean('is_active')->default(1);
+        Schema::create('demultiplexer', function(Blueprint $table){
 
-                $table->timestamps();
-            });
-        }
+            $table->binaryUuid('demultiplexer_id')->primary();
+            $table->text('params');
+            $table->string('token',30)->unique();
+            $table->boolean('is_active')->default(1);
+
+            $table->timestamps();
+        });
     }
 
     /**
